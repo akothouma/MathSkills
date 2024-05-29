bb2751aa5b52f7d2105103e02d1b7158eaca2441
lakoth <lornaakothpb@gmail.com>
2024-05-29 18:29:50 +0300

    feat:check existence of file

--- MathAndStats.java
+++ MathAndStats.java
@@ -1,48 +1,49 @@
 import java.io.*; //FileReader,Scanner classes
-import java.lang.*; //Math class
-import java.util.*; //Collections class
+import java.lang.Math; //Math class
+import java.util.ArrayList;
+import java.util.Scanner;//scanner class
+import java.util.Collections; //Collections class
 
  public class MathAndStats{
 
-    static int calcAvarage( ArrayList<Float> ints){
-        Float sum=0.0f;
-        int average=0;
+    static double calcAvarage( ArrayList<Double> ints){
+        double sum=0.0;
+        double average=0.0;
         int inputSize=ints.size();
         for(int i=0;i<inputSize;i++){
-            sum=sum+i;
+            sum+=ints.get(i);
         }
-        average=Math.round(sum/(float)inputSize);
+        average=sum/(double)inputSize;
+        //System.out.println(average);
         return average;
         }
    
-    static int calcMedian(ArrayList<Float> ints){
+    static int calcMedian(ArrayList<Double> ints){
         int inputSize=ints.size(),median=0;
         Collections.sort(ints);
         if (inputSize%2==1){
-              median=Math.round(ints.get(inputSize/2));
+              median=(int)Math.round(ints.get(inputSize/2));
              return median; 
         }else{
-            median=Math.round((ints.get(inputSize/2)+ ints.get(inputSize/2-1))/2);
+            median=(int)Math.round((ints.get(inputSize/2)+ ints.get(inputSize/2-1))/2);
           return median;
             
         }
         
     }
-    static int calcVariance(ArrayList<Float> ints){
-        int deviation=0,variance=0;
+    static double calcVariance(ArrayList<Double> ints,double average){
+        double deviation=0.0,variance=0.0;
         int inputSize=ints.size();
-        int average=calcAvarage(ints);
-        for(Float i:ints){
-             deviation+=Math.pow(Math.round(i)-average,2);
+        for(Double i:ints){
+             deviation+=Math.pow((i-average),2);
              }
-             variance=Math.round(deviation/inputSize);
+             variance=deviation/(double)inputSize;
              
       return variance;
     }
-    static int calcStandardDeviation(ArrayList<Float> ints){ 
+    static int calcStandardDeviation(ArrayList<Double> ints, double variance){ 
         int standardDeviation=0;
-        int variance=calcVariance(ints);
-        standardDeviation=(int)Math.sqrt(variance);
+        standardDeviation=(int)Math.round(Math.sqrt(variance));
         return standardDeviation;
     }
 
@@ -50,31 +51,47 @@ import java.util.*; //Collections class
 
     //main method
     public static void main(String[] args)throws Exception{      
-     if (args.length<0){
+     if (args.length==0){
             System.out.println("No arguments provided");
         }else if (args.length>1) {
             System.out.println("Many arguments provided");
         }else{
             String file=args[0];
-            ArrayList<Float> fileContent=new ArrayList<>();
-     
-            try{
-             Scanner s = new Scanner(new FileReader(file));
-             while(s.hasNext()){
-              fileContent.add(s.nextFloat());
-              }
-             s.close();
-            }catch (Exception e){
-               System.out.println("Problem reading the file");
+            int ext=file.lastIndexOf(".");
+           if (file.lastIndexOf(".")!=-1){
+              String extension =file.substring(ext);
+              if (extension.equals(".txt")){
+                  ArrayList<Double> fileContent=new ArrayList<>();
+                  try{
+                   Scanner s = new Scanner(new FileReader(file));
+                   while(s.hasNext()){
+                          fileContent.add(s.nextDouble());
+                    }
+                   s.close();
+                 //  System.out.println(fileContent);
+                  }catch (Exception e){
+                     System.out.println("Problem reading the file");
+                     return;
+                   }
+                   double average=calcAvarage(fileContent);
+                   double variance=calcVariance(fileContent,average);
+                   System.out.printf("Average: %d\n",Math.round(average));
+                   System.out.printf("Median: %d\n",calcMedian(fileContent));
+                   System.out.printf("Variance: %d\n",Math.round(variance));
+                   System.out.printf("Standard Deviation: %d\n",calcStandardDeviation(fileContent,variance));    
+      
+               }else{
+                System.out.println("Files should be of .txt extension");
+                return;  
+               
+              }   
+                }else{
+                    System.out.println("include file extension");
+                }
+               
              }
-             System.out.printf("Average: %d\n",calcAvarage(fileContent));
-             System.out.printf("Median: %d\n",calcMedian(fileContent));
-             System.out.printf("Variance: %d\n",calcVariance(fileContent));
-             System.out.printf("Standard Deviation: %d\n",calcStandardDeviation(fileContent));    
-
-         }  
-         
-        }   
+            }
+            
     }
     
 
